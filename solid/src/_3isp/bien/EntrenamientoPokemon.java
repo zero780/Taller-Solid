f6c487182ce17e2b42ffcec82722bc079bce1bb5
=zero780 <milton.garcia1998@hotmail.com>
2018-10-30 15:03:45 -0500

    Aplicación de ISP

---
+++ solid/src/_3isp/bien/EntrenamientoPokemon.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _3isp.bien;
+
+/**
+ * 
+ * @author Palacios
+ */
+
+/*
+    Esta es una "interfaz gorda" ya que tiene muchas responsabilidades
+*/
+public interface EntrenamientoPokemon {
+    
+    
+
+    void aprenderAtaqueCorte();   //todo pokemon tiene esta habilidad
+    
+    
+}
