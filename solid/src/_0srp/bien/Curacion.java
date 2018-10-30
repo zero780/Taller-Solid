9945aeb8d63bd12d59aa67c3f6462a68b4898585
Administrador <Administrador@GGIECUT1WRK040.espol.edu.ec>
2018-10-30 13:28:59 -0500

    Creacion de clase curacion para el correcto uso de SRP

---
+++ solid/src/_0srp/bien/Curacion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _0srp.bien;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Curacion {
+    public void curar(Pokemon pokemon){
+        pokemon.setPorcentajeVida(100);
+    }
+}
